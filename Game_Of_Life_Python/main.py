d7699a5be9a5a3a83a989bed5406fe616febd1e3
Mateusz-best-creator <szachmat2004@gmail.com>
2024-04-12 12:25:49 +0200

    Adding basic for starting page

--- Game_Of_Life_Python/main.py
+++ Game_Of_Life_Python/main.py
@@ -3,6 +3,5 @@ import pygame
 
 if __name__ == "__main__":
     world = World()
-    world.initialize_organisms()
     world.run()
     pygame.quit()
--- Game_Of_Life_Python/world.py
+++ Game_Of_Life_Python/world.py
@@ -28,28 +28,45 @@ class World:
 
         # Pygame stuff
         pygame.init()
-        self.screen = pygame.display.set_mode((screen_height, screen_width))
+        self.screen = pygame.display.set_mode((screen_height, screen_width)) # screen is our main surface
         self.clock = pygame.time.Clock()
         self.running = True
 
         # Font stuff
-        self.font = pygame.font.SysFont(None, 24)
-        self.font1 = pygame.font.SysFont("chalkduster.ttf", 72)
-        self.title = self.font1.render("World Simulation", True, "#333333")
+
+        # Title font
+        self.font_title = pygame.font.SysFont("chalkduster.ttf", 72)
+        self.title = self.font_title.render("World Simulation", True, "#333333")
+        self.title_width = self.title.get_width()
+        self.title_x = (self.screen_width - self.title_width) / 2
+        self.title_height = self.screen_height * 0.1
 
         # Organisms stuff
         self.ORGANISMS_TYPES = 12
         self.organisms = []
 
-        # Board stuff
-        self.height = 10
-        self.width = 10
+        # Initial screen stuff
+        self.option_rectangle_width = self.screen_width / 3 - 50
+        self.option_rectangle_height = self.screen_height * 0.15
+        self.option_rectangle_offset = (self.screen_width - 3 * self.option_rectangle_width) / 4
+        self.option_rectangle_left = []
+        self.option_rectangle_top = self.screen_height * 0.7
+        for i in range(1, 3+1):
+            self.option_rectangle_left.append(self.option_rectangle_offset * i + (i - 1) * self.option_rectangle_width)
+
+        # Option font
+        self.font_option = pygame.font.SysFont("chalkduster.ttf", 24)
+        self.option_font_color = "#333333"
+        self.options_font = [self.font_option.render("Start Simulation", True, self.option_font_color), 
+                        self.font_option.render("Load Simulation", True, self.option_font_color), 
+                        self.font_option.render("Quit Simulation", True, self.option_font_color)]
+        self.options_font_left = [self.option_rectangle_left[i] + self.option_rectangle_width * 0.11 for i in range(3)]
+        self.options_font_top = self.option_rectangle_top + self.option_rectangle_height * 0.4
 
     def initialize_organisms(self):
         for index in range(1, self.ORGANISMS_TYPES + 1):
 
             random_amount = random.randrange(1, 3 + 1)
-            print("RAnd = ", random_amount)
             
             # Map index to a type name
             type = OrganismType(index)
@@ -81,16 +98,14 @@ class World:
                 self.organisms.append(Human("Human", 1, 1))
     
     def run(self):
+        self.initialize_organisms()
+        self.draw_starting_screen()
         while self.running:
             # poll for events
             # pygame.QUIT event means the user clicked X to close your window
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
-
-            self.screen.fill("#ffffff")
-            self.screen.blit(self.title, (200, 40))
-
 
             pygame.display.flip()
             self.clock.tick(60)
@@ -99,7 +114,16 @@ class World:
     def add_organisms(self, times, name, object_type):
         pass
 
+    def draw_starting_screen(self):
+        self.screen.fill("#ffffff")
+        self.screen.blit(self.title, (self.title_x, self.title_height))
+        # Draw all 3 option rectangles and all 3 text options
+        for i in range(3):
+            pygame.draw.rect(self.screen, 
+                             "#fc0101", 
+                             pygame.Rect(self.option_rectangle_left[i], self.option_rectangle_top, self.option_rectangle_width, self.option_rectangle_height), 2)
 
+            self.screen.blit(self.options_font[i], (self.options_font_left[i], self.options_font_top))
 
 
 
