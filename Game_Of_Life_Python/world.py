5c6a7e00d1489715edc66c1584987539507fa6a1
Mateusz-best-creator <114943990+Mateusz-best-creator@users.noreply.github.com>
2024-04-21 09:37:26 +0200

    Adding instructions in the start page

--- Game_Of_Life_Python/world.py
+++ Game_Of_Life_Python/world.py
@@ -52,10 +52,6 @@ class World:
         self.title_left = (self.screen_width - self.title_width) / 2
         self.title_top = self.screen_height * 0.1
 
-        # Organisms stuff
-        self.ORGANISMS_TYPES = 12
-        self.organisms = []
-
         # Initial screen stuff
         self.option_rectangle_width = self.screen_width / 3 - 50
         self.option_rectangle_height = self.screen_height * 0.15
@@ -78,9 +74,13 @@ class World:
                                  "Load Simulation", True, self.option_font_color),
                              self.font_option.render("Quit Simulation", True, self.option_font_color)]
         self.options_font_left = [self.option_rectangle_left[i] +
-                                  self.option_rectangle_width * 0.11 for i in range(3)]
+                                  self.option_rectangle_width * 0.1 for i in range(3)]
         self.options_font_top = self.option_rectangle_top + \
             self.option_rectangle_height * 0.4
+
+        # Organisms stuff
+        self.ORGANISMS_TYPES = 12
+        self.organisms = []
 
     def initialize_organisms(self):
         for index in range(1, self.ORGANISMS_TYPES + 1):
@@ -121,31 +121,33 @@ class World:
         self.draw_starting_screen()
         while self.running:
             # poll for events
-            # pygame.QUIT event means the user clicked X to close your window
             for event in pygame.event.get():
+                # pygame.QUIT event means the user clicked X to close your window
                 if event.type == pygame.QUIT:
                     self.running = False
                 elif event.type == pygame.MOUSEBUTTONUP:
                     pos = pygame.mouse.get_pos()
 
                     click_x, click_y = pos
-                    print("(x, y) = ", click_x, click_y)
+                    # print("(x, y) = ", click_x, click_y)
                     if click_y >= self.option_rectangle_top and click_y <= self.option_rectangle_top + self.option_rectangle_height:
                         for index, option_left_distance in enumerate(self.option_rectangle_left):
                             if click_x >= option_left_distance and click_x <= option_left_distance + self.option_rectangle_width:
                                 self.chosen_option = OptionType(index + 1)
-                                print("Chosen option = ",
-                                      self.chosen_option.name)
-                                if self.chosen_option == OptionType.QUIT_SIMULATION:
-                                    self.running = False
-                                elif self.chosen_option == OptionType.PLAY_SIMULATION:
-                                    pass
-                                elif self.chosen_option == OptionType.LOAD_SIMULATION:
-                                    pass
 
             pygame.display.flip()
             self.clock.tick(60)
             pygame.display.update()
+
+            if self.chosen_option == OptionType.PLAY_SIMULATION:
+                self.play_simulation()
+                self.draw_starting_screen()
+            elif self.chosen_option == OptionType.LOAD_SIMULATION:
+                self.load_simulation()
+                self.draw_starting_screen()
+            elif self.chosen_option == OptionType.QUIT_SIMULATION:
+                self.running = False
+            self.chosen_option = OptionType.NONE
 
     def add_organisms(self, times, name, object_type):
         pass
@@ -153,11 +155,58 @@ class World:
     def draw_starting_screen(self):
         self.screen.fill("#ffffff")
         self.screen.blit(self.title, (self.title_left, self.title_top))
+
+        # Text instructions
+        instruction_lines = ["During the game press P to go to the next turn. If",
+                             "human is alive you will be asked to press one of, the",
+                             "arrow keys to move. If human is not alive the game will",
+                             "play and the journal will be shown on the left side."]
+        instruction_font = pygame.font.SysFont("chalkduster.ttf", 24)
+        instruction_font_color = "#333333"
+        instruction_font_text = [instruction_font.render(
+            instruction_lines[i], True, instruction_font_color) for i in range(len(instruction_lines))]
+        instruction_width = instruction_font_text[len(
+            instruction_lines) - 1].get_width()
+        instruction_font_left = (self.screen_width - instruction_width) / 2
+        INSTRUCTION_FONT_GAP = 18
+        instruction_font_top = [self.screen_height * 0.4 +
+                                INSTRUCTION_FONT_GAP * i for i in range(len(instruction_lines))]
+        instruction_width = instruction_font_text[len(
+            instruction_lines) - 1].get_width()
+
         # Draw all 3 option rectangles and all 3 text options
         for i in range(3):
             pygame.draw.rect(self.screen,
-                             "#fc0101",
+                             "#000000",
                              pygame.Rect(self.option_rectangle_left[i], self.option_rectangle_top, self.option_rectangle_width, self.option_rectangle_height), 2)
+            # Subtracting 4 to account for the border thickness
+            inner_rect_width = self.option_rectangle_width - 4
+            inner_rect_height = self.option_rectangle_height - 4
+            # Adding 2 to offset the border
+            inner_rect_left = self.option_rectangle_left[i] + 2
+            inner_rect_top = self.option_rectangle_top + 2
+            pygame.draw.rect(self.screen,
+                             "#c0c0c0",
+                             pygame.Rect(inner_rect_left, inner_rect_top, inner_rect_width, inner_rect_height))
 
             self.screen.blit(
                 self.options_font[i], (self.options_font_left[i], self.options_font_top))
+        for i in range(len(instruction_lines)):
+            self.screen.blit(
+                instruction_font_text[i], (instruction_font_left, instruction_font_top[i]))
+
+    def play_simulation(self):
+        game_running = True
+        self.screen.fill("#111141")
+        print("running game!")
+        while game_running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    game_running = False
+
+            pygame.display.flip()
+            self.clock.tick(60)
+            pygame.display.update()
+
+    def load_simulation(self):
+        pass
